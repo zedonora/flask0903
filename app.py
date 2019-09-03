73517641ceb37faaf474907ea1f909e3b4aa850f
zedonora <timeundtime@gmail.com>
2019-09-04 06:28:09 +0900

    second

--- app.py
+++ app.py
@@ -52,21 +52,21 @@ def hello():
     return render_template('hello.html', userid=userid)
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+dbfile = os.path.join(basedir, 'db.sqlite')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'qwewqeqweqweeqweqw'
+
+csrf = CSRFProtect()
+csrf.init_app(app)
+
+db.init_app(app)
+db.app = app
+db.create_all()
+
 if __name__ == "__main__":
-
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    dbfile = os.path.join(basedir, 'db.sqlite')
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
-    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = 'qwewqeqweqweeqweqw'
-
-    csrf = CSRFProtect()
-    csrf.init_app(app)
-
-    db.init_app(app)
-    db.app = app
-    db.create_all()
 
     app.run(host='127.0.0.1', port=5000, debug=True)
